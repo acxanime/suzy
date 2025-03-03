8274316c9dc104b3f96e46bc73f1b2f8d5a2fc27
acxanime <acxanime07@gmail.com>
2025-03-03 21:28:19 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -27,7 +27,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             pass
     elif data == "buy_prem":
         await query.message.edit_text(
-            text=f"<b><u>ᴘʀᴇᴍɪᴜᴍ ʙᴇɴɪғɪᴛs</u> ᴀɴᴅ ᴘʀɪᴄᴇs\n\n● ᴅɪʀᴇᴄᴛ ᴄʜᴀɴɴᴇʟ ʟɪɴᴋs\n● ɴᴏ ᴀᴅ ʟɪɴᴋs\n\n🎖️ ᴘʀɪᴄɪɴɢ ʀᴀᴛᴇs :\n\n● 7 ᴅᴀʏs - ɪɴʀ {PRICE1}\n\n● 1 ᴍᴏɴᴛʜ - ɪɴʀ {PRICE2}\n\n● 3 ᴍᴏɴᴛʜ - ɪɴʀ {PRICE3}\n\n● 6 ᴍᴏɴᴛʜ - ɪɴʀ {PRICE4}\n\n● 12 ᴍᴏɴᴛʜs - ɪɴʀ {PRICE5} \n\n\n💵 UPI ID -  <code>{UPI_ID}</code>\n\n\n📸 QR - ᴄʟɪᴄᴋ ʜᴇʀᴇ ᴛᴏ ꜱᴄᴀɴ ({UPI_IMAGE_URL})\n\n♻️ If payment is not getting sent on above given QR code then inform admin, he will give you new QR code\n\n\n‼️ Must Send Screenshot after payment\n\nғᴏʀ ᴘᴀʏᴍᴇɴᴛ ᴅᴍ @AcxAnime | @sitaratoons_support</b>",
+            text=f"<b><u>ᴘʀᴇᴍɪᴜᴍ ʙᴇɴɪғɪᴛs ᴀɴᴅ ᴘʀɪᴄᴇs</u> \n\n● ᴅɪʀᴇᴄᴛ ᴄʜᴀɴɴᴇʟ ʟɪɴᴋs\n● ɴᴏ ᴀᴅ ʟɪɴᴋs\n\n🎖️ ᴘʀɪᴄɪɴɢ ʀᴀᴛᴇs :\n\n● 7 ᴅᴀʏs - ɪɴʀ {PRICE1}\n\n● 1 ᴍᴏɴᴛʜ - ɪɴʀ {PRICE2}\n\n● 3 ᴍᴏɴᴛʜ - ɪɴʀ {PRICE3}\n\n● 6 ᴍᴏɴᴛʜ - ɪɴʀ {PRICE4}\n\n● 12 ᴍᴏɴᴛʜs - ɪɴʀ {PRICE5} \n\n\n💵 UPI ID -  <code>{UPI_ID}</code>\n\n\n📸 QR - ᴄʟɪᴄᴋ ʜᴇʀᴇ ᴛᴏ ꜱᴄᴀɴ ({UPI_IMAGE_URL})\n\n♻️ If payment is not getting sent on above given QR code then inform admin, he will give you new QR code\n\n\n‼️ Must Send Screenshot after payment\n\nғᴏʀ ᴘᴀʏᴍᴇɴᴛ ᴅᴍ @AcxAnime | @sitaratoons_support</b>",
             disable_web_page_preview=True,
             reply_markup = InlineKeyboardMarkup(
                 [   
